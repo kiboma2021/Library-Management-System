782b5e9008eaf90e2b609a0097876d7f5dd4af57
kiboma <omayioben@gmail.com>
2022-10-25 11:48:52 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'book/list'
+  get 'book/new'
+  post 'book/create'
+  patch 'book/update'
+  get 'book/show'
+  get 'book/edit'
+  get 'book/delete'
+  get 'book/update'
+  get 'book/show_subjects'
 end
