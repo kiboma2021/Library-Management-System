eda29ff890f8d2cddf14e42382017c105999ad90
kiboma <omayioben@gmail.com>
2022-10-25 15:24:52 +0300

    Add new book form

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  get 'book/list'
+  root "book#list"
   get 'book/new'
   post 'book/create'
   patch 'book/update'
